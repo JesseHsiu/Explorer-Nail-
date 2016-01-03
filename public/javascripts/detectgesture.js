abcc8bb1aae9f17a5718a4cf9848e8224a474b41
JesseHsiu <jesse801122@gmail.com>
2016-01-03 16:34:39 +0800

    add filter for end

--- public/javascripts/detectgesture.js
+++ public/javascripts/detectgesture.js
@@ -8,7 +8,13 @@ var SG_values = [[],
                  [],
                  []];
 
+var stateMachine = {
+  IDLE: 0,
+  START: 1,
+  END: 2,
+};
 
+var state = stateMachine.IDLE;
 
 var socket = io('http://localhost:3000');
 socket.on('identification', function (data) {
@@ -16,45 +22,67 @@ socket.on('identification', function (data) {
   socket.emit('identification', { device: 'desktop' });
 });
 
-var state = 'Idle';
-var datalength = 50;
+// var state = 'Idle';
+var collect_datalength = 50;
+var activate_datalength = 25;
 
+
+var recount = 0;
 socket.on('SGdata', function (data) {
   console.log('data');
   var sgValues = data['data'];
   $('#value').text(sgValues);
 
-  var showDetect = false;
+  var startDetect = false;
+  var endDetect = false;
   for (var i = 0; i < sgValues.length; i++) {
     SG_values[i].push(sgValues[i]);
-    if (SG_values[i].length > datalength) {
+    if (SG_values[i].length > collect_datalength) {
       SG_values[i].shift();
 
+      if (state == stateMachine.IDLE)
+      {
+        var act_front = SG_values[i].slice(0, activate_datalength/2) 
+        var act_end = SG_values[i].slice(activate_datalength/2, activate_datalength) 
+        
+        var act_frontavg = getAvg(act_front);
+        var act_endavg = getAvg(act_end);
 
+        if (act_endavg/act_frontavg > 1.2 || act_endavg/act_frontavg < 0.8) {
+          startDetect = true;
+          for (var i = 0; i < SG_values.length; i++) {
+            SG_values[i] = [];
+          };
+        }
+      }
+      else if(state == stateMachine.START){
+        var front = SG_values[i].slice(0, collect_datalength/2) 
+        var end = SG_values[i].slice(collect_datalength/2, collect_datalength) 
+        
+        var frontavg = getAvg(front);
+        var endavg = getAvg(end);
 
-      var front = SG_values[i].slice(0, datalength/2) 
-      var end = SG_values[i].slice(datalength/2, datalength) 
-      
-      var frontavg = getAvg(front);
-      var endavg = getAvg(end);
-
-      // console.log(i);
-      // console.log(frontavg);
-      // console.log();
-
-      if (endavg/frontavg > 1.2 || endavg/frontavg < 0.8) {showDetect = true;}
-      // else if (endavg/frontavg < 1.1 && endavg/frontavg > 0.9) {};
-
+        if (endavg/frontavg < 1.01 && endavg/frontavg > 0.99) {endDetect = true;}
+        
+      }
     };
-
   };
 
-  if (showDetect) {
-    state = 'Start Gesture'; $('#state').text('Current State: ' + state);
+  if (startDetect) {state = stateMachine.START;}
+  else if(endDetect){state = stateMachine.IDLE;};
+
+
+  if (state == stateMachine.IDLE) {
+    $('#state').text('Current State: Idle');
   }
-  else{
-    state = 'Idle'; $('#state').text('Current State: ' + state);
-  };
+  else if(state == stateMachine.START)
+  {
+    $('#state').text('Current State: Start');
+  }
+  else if(state == stateMachine.END)
+  {
+    $('#state').text('Current State: End');
+  }
 
 });
 
