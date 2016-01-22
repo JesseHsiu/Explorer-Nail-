77137e09a57f7857e422ef6b91ab9be50986cd31
王權 <timowang1991@gmail.com>
2016-01-22 17:07:38 +0800

    add chart to training page

--- public/javascripts/learning_client.js
+++ public/javascripts/learning_client.js
@@ -1,3 +1,102 @@
+
+var SG_values = [[0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0]];
+
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        columns: [
+            ['SG1', 0],
+            ['SG2', 0],
+            ['SG3', 0],
+            ['SG4', 0],
+            ['SG5', 0],
+            ['SG6', 0],
+            ['SG7', 0],
+            ['SG8', 0],
+            ['SG9', 0]
+        ],
+        type: 'bar',
+        labels: true
+    },
+    bar: {
+        width: {
+            ratio: 0.8
+        }
+    },
+    interaction: {
+     enabled: false
+   },
+   axis: {
+     y: {
+      min: -512,
+      max: 512
+     }
+   },
+   grid: {
+        y: {
+            lines: [{value:0}]
+        }
+    },
+    transition: {
+     duration: 0
+   },
+    tooltip: {
+      show: false
+    }
+});
+
+window.setInterval(function(){
+
+  $.ajax({
+      url: 'http://127.0.0.1:3000/SGValues',
+      dataType: "jsonp",
+      jsonpCallback: "_SGValues",
+      cache: false,
+      timeout: 5000,
+      success: function(data) {
+        // console.log(data)
+        obj = JSON.parse(data);
+        var values = obj.values.split(",");
+
+        // console.log((['SG1']).concat(SG_values[0]));
+        
+        for (var i = 0; i < values.length; i++) {
+         SG_values[i].push(values[i]);
+         SG_values[i].shift();
+
+         SG_values[i].unshift('SG' + String(i+1));
+         SG_values[i].shift();
+
+        };
+
+        chart.load({
+        columns: [
+          ['SG1', SG_values[0][9]-512],
+            ['SG2', SG_values[1][9]-512],
+            ['SG3', SG_values[2][9]-512],
+            ['SG4', SG_values[3][9]-512],
+            ['SG5', SG_values[4][9]-512],
+            ['SG6', SG_values[5][9]-512],
+            ['SG7', SG_values[6][9]-512],
+            ['SG8', SG_values[7][9]-512],
+            ['SG9', SG_values[8][9]-512]
+        ]
+      });
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+      }
+  });
+}, 100);
+
 var record_state = {
   IDLE: 0,
   RECORDING: 1,
@@ -28,6 +127,8 @@ function recordBtnClick () {
 			state = record_state.IDLE;
 			$("#recordBtn").removeClass("btn-danger");
 			$("#recordBtn").text("Start Record");
+
+         
 	    });
 	}
 }
